c09e4b113e09e272f5b87ce2aa96a5ddf1c0eb83
CifWang <wcf@nssc.ac.cn>
2018-09-17 19:51:04 +0800

    test Java and Matlab

--- src/javamatlabTests/TestMyAdd.java
+++ src/javamatlabTests/TestMyAdd.java
@@ -1,5 +1,18 @@
 package javamatlabTests;
 
+import com.mathworks.toolbox.javabuilder.MWClassID;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+
+import myAdd.JMAdd;;
 public class TestMyAdd {
+	public static void main(String[] args) throws MWException {
+		MWNumericArray a=new MWNumericArray(Double.valueOf(1),MWClassID.DOUBLE);
+		MWNumericArray b=new MWNumericArray(Double.valueOf(2),MWClassID.DOUBLE);
+		
+		JMAdd myadd=new JMAdd();
+		Object[] r=myadd.myAdd(1, a,b);
+		System.out.println(r[0]);
+	}
 
 }
